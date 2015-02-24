be62f68fb7a86d3032abf4a0db6457825217bfab
dragonator <draganov93@gmail.com>
2015-02-25 01:55:04 +0200

    Add basic GUI

--- main.py
+++ main.py
@@ -1,35 +1,28 @@
-__version__ = '1.0'
-import sys
-
+import kivy
+kivy.require('1.8.0')
 from kivy.app import App
-from kivy.lang import Builder
-
-kv = '''
-BoxLayout:
-    orientation: 'vertical'
-    Button:
-        text: 'SYNC PLAYLISTS LIST'
-        on_press: sys.exit()
-        size_hint_y: None
-        height: '55dp'
-    Button:
-        text: 'EDIT SYNC LIST'
-        on_press: sys.exit()
-        size_hint_y: None
-        height: '55dp'
-    TextInput:
-        height: '55dp'
-    Button:
-        text: 'DOWNLOAD PLAYLIST'
-        on_press: sys.exit()
-        size_hint_y: None
-        height: '55dp'
-'''
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+__version__ = '1.0'
 
 
 class TestApp(App):
+
     def build(self):
-        return Builder.load_string(kv)
+        layout = BoxLayout(orientation='vertical')
+        sync_button = Button(text='SYNC PLAYLISTS LIST')
+        edit_button = Button(text='EDIT SYNC LIST')
+        url_input = TextInput()
+        download_button = Button(text='DOWNLOAD PLAYLIST')
+
+        layout.add_widget(sync_button)
+        layout.add_widget(edit_button)
+        layout.add_widget(url_input)
+        layout.add_widget(download_button)
+
+        return layout
+
 
 if __name__ == '__main__':
     TestApp().run()
